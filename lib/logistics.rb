b0b4c6e8e7c4434b052a25119ce7abf7ce6a20ed
steve21168 <steve21168@gmail.com>
2016-04-10 00:29:39 -0400

    Cleaned up game

--- lib/logistics.rb
+++ lib/logistics.rb
@@ -1,87 +1,88 @@
-def loading(page_num)
-  icons = ['◐','◓','◑','◒']
-  percent = "#{((page_num / 9.0) * 100).round(0)}%"
+class Logistic
+  attr_accessor :insult_count
 
-  icons.each do |icon|
-    print "              ------#{icon} Loading #{percent} #{icon}------ \r"
-    sleep 0.25
+  def initialize
+    @insult_count = 0
   end
-end
 
-def complete
-  system 'clear'
-  print "                               100% Complete"
-  sleep 1
-end
+  def prompt
+    system 'clear'
+    puts "                Which character would you like to learn about?"
+  end
 
-def prompt
-  system 'clear'
-  puts "                Which character would you like to learn about?"
-end
+  def fav_char
+    gets.chomp.downcase
+  end
 
-def fav_char
-  gets.chomp.downcase
-end
+  def insult
+    system 'clear'
+    self.insult_count += 1
 
-def insult
-  system 'clear'
-  puts "\n              Oh please, have you ever seen star wars? \n\n"
-end
-
-def know_more
-  puts "\n\nWould you like to no more?"
-  gets.chomp
-end
-
-def show_character_info(characters, fav_char)
-  characters.each do |character|
-    if character.name.downcase == fav_char
-      puts_attributes(character)
-      sleep 4
-      return
+    case insult_count
+    when 1
+      puts "                Oh please, have you ever seen star wars? \n\n"
+    when 2
+      puts "                Now you're starting to annoy me."
+    else
+      puts "                Smh smh.. "
     end
   end
-  raise error
-end
 
-def run_user_interaction(characters)
-  loop do
-    begin
-      prompt
-      show_character_info(characters, fav_char)
-    rescue
-      insult
-      sleep 2
-      retry
-    end
-    break if /^no/ === know_more 
+  def know_more
+    puts "\n\nWould you like to no more?"
+    gets.chomp
   end
-end
 
-def puts_attributes(character)
-  puts "\n*** #{character.name} ***"
-  puts "The man, the myth, the legend: \n\n"
-  puts "Height: #{character.height}"
-  puts "Mass: #{character.mass}"
-  puts "Hair Color: #{character.hair_color}"
-  puts "Skin Color: #{character.skin_color}"
-  puts "Eye Color: #{character.eye_color}"
-  puts "Date of birth: #{character.birth_year}"
-  puts "Gender: #{character.gender}"
-end
+  def show_character_info(characters, fav_char)
+    characters.each do |character|
+      if character.name.downcase == fav_char
+        puts_attributes(character)
+        sleep 4
+        return
+      end
+    end
+    raise error
+  end
 
-def print_logo
-  puts "           ________________.  ___     .______ "
-  puts "          /                | /   \\    |   _  \\"
-  puts "         |   (-----|  |----`/  ^  \\   |  |_)  |"
-  puts "          \\   \\    |  |    /  /_\\  \\  |      /"
-  puts "     .-----)   |   |  |   /  _____  \\ |  |\\  \\-------."
-  puts "     |________/    |__|  /__/     \\__\\| _| `.________|"
-  puts "      ____    __    ____  ___     .______    ________."
-  puts "      \\   \\  /  \\  /   / /   \\    |   _  \\  /        |"
-  puts "       \\   \\/    \\/   / /  ^  \\   |  |_)  ||   (-----`"
-  puts "        \\            / /  /_\\  \\  |      /  \\   \\"
-  puts "         \\    /\\    / /  _____  \\ |  |\\  \\---)   |"
-  puts "          \\__/  \\__/ /__/     \\__\\|__| `._______/"
-  puts "\n\n"
+  def run_user_interaction(characters)
+    loop do
+      begin
+        prompt
+        show_character_info(characters, fav_char)
+      rescue
+        insult
+        sleep 2
+        retry
+      end
+      break if /^no/ === know_more
+    end
+  end
+
+  def puts_attributes(character)
+    puts "\n                            *** #{character.name} ***"
+    puts "                       The man, the myth, the legend: \n\n"
+    puts "                       Height: #{character.height}"
+    puts "                       Mass: #{character.mass}"
+    puts "                       Hair Color: #{character.hair_color}"
+    puts "                       Skin Color: #{character.skin_color}"
+    puts "                       Eye Color: #{character.eye_color}"
+    puts "                       Date of birth: #{character.birth_year}"
+    puts "                       Gender: #{character.gender}"
+  end
+
+  def print_logo
+    puts "           ________________.  ___     .______ "
+    puts "          /                | /   \\    |   _  \\"
+    puts "         |   (-----|  |----`/  ^  \\   |  |_)  |"
+    puts "          \\   \\    |  |    /  /_\\  \\  |      /"
+    puts "     .-----)   |   |  |   /  _____  \\ |  |\\  \\-------."
+    puts "     |________/    |__|  /__/     \\__\\| _| `.________|"
+    puts "      ____    __    ____  ___     .______    ________."
+    puts "      \\   \\  /  \\  /   / /   \\    |   _  \\  /        |"
+    puts "       \\   \\/    \\/   / /  ^  \\   |  |_)  ||   (-----`"
+    puts "        \\            / /  /_\\  \\  |      /  \\   \\"
+    puts "         \\    /\\    / /  _____  \\ |  |\\  \\---)   |"
+    puts "          \\__/  \\__/ /__/     \\__\\|__| `._______/"
+    puts "\n\n"
+  end
 end
--- lib/star_wars_api.rb
+++ lib/star_wars_api.rb
@@ -5,7 +5,7 @@ class StarWarsApi
     page_num = 0
 
     loop do
-      loading(page_num)
+      self.loading(page_num)
       response = RestClient.get(url)
       parsed = JSON.parse(response)
       results << parsed["results"]
@@ -13,10 +13,26 @@ class StarWarsApi
       page_num += 1
       break unless parsed["next"]
     end
-    complete
+    self.complete
     results.flatten!
 
     return results unless block_given?
     results.map {|result| yield(result)}
+  end
+
+  def self.loading(page_num)
+    icons = ['◐','◓','◑','◒']
+    percent = "#{((page_num / 9.0) * 100).round(0)}%"
+
+    icons.each do |icon|
+      print "               ------#{icon} Loading #{percent} #{icon}------ \r"
+      sleep 0.25
+    end
+  end
+
+  def self.complete
+    system 'clear'
+    print "Complete!"
+    sleep 1
   end
 end
